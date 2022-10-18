4a75a406139d4e1510e79f7fe072ce130dbd13dc
Sergio Álvarez <sergio.alvarez@digitalvirgo.com>
2022-10-18 11:45:32 +0200

    config split

--- plex_generate_previews.py
+++ plex_generate_previews.py
@@ -1,34 +1,24 @@
 #!/usr/bin/env python3
 
-# EDIT These Vars #
-PLEX_URL = 'https://xxxxxx.plex.direct:32400/' # If running locally, can also enter IP directly "https://127.0.0.1:32400/"
-PLEX_TOKEN = 'xxxxxx'
-PLEX_BIF_FRAME_INTERVAL = 5
-THUMBNAIL_QUALITY = 4 # Allowed range is 2 - 6 with 2 being highest quality and largest file size and 6 being lowest quality and smallest file size. # 
-PLEX_LOCAL_MEDIA_PATH = '/path_to/plex/Library/Application Support/Plex Media Server/Media'
-TMP_FOLDER = '/dev/shm/plex_generate_previews'
-
-PLEX_LOCAL_VIDEOS_PATH_MAPPING = '/path/this/script/sees/to/video/library'
-PLEX_VIDEOS_PATH_MAPPING = '/path/plex/sees/to/video/library'
-
-GPU_THREADS = 4
-CPU_THREADS = 4
-
-# DO NOT EDIT BELOW HERE #
+import config
 
 import os
 import sys
 from concurrent.futures import ProcessPoolExecutor
 import re
+import time
 import subprocess
 import shutil
 import multiprocessing
 import glob
-import os
 import struct
+import array
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 if not shutil.which("mediainfo"):
     print('MediaInfo not found.  MediaInfo must be installed and available in PATH.')
-    sys.exit(1)	
+    sys.exit(1)
 try:
     from pymediainfo import MediaInfo
 except ImportError:
@@ -39,13 +29,11 @@ try:
 except ImportError:
     print('Dependencies Missing!  Please run "pip3 install gpustat".')
     sys.exit(1)
-import time
 try:
     import requests
 except ImportError:
     print('Dependencies Missing!  Please run "pip3 install requests".')
     sys.exit(1)
-import array
 try:
     from plexapi.server import PlexServer
 except ImportError:
@@ -73,22 +61,19 @@ if not FFMPEG_PATH:
 
 console = Console(color_system=None, stderr=True)
 
-import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
 
 def generate_images(video_file_param, output_folder, lock):
-    video_file = video_file_param.replace(PLEX_VIDEOS_PATH_MAPPING, PLEX_LOCAL_VIDEOS_PATH_MAPPING)
+    video_file = video_file_param.replace(config.PLEX_VIDEOS_PATH_MAPPING, config.PLEX_LOCAL_VIDEOS_PATH_MAPPING)
     media_info = MediaInfo.parse(video_file)
-    vf_parameters = "fps=fps={}:round=up,scale=w=320:h=240:force_original_aspect_ratio=decrease".format(round(1 / PLEX_BIF_FRAME_INTERVAL, 6))
-    
+    vf_parameters = "fps=fps={}:round=up,scale=w=320:h=240:force_original_aspect_ratio=decrease".format(round(1 / config.PLEX_BIF_FRAME_INTERVAL, 6))
+
     # Check if we have a HDR Format. Note: Sometimes it can be returned as "None" (string) hence the check for None type or "None" (String)
     if media_info.video_tracks[0].hdr_format != "None" and media_info.video_tracks[0].hdr_format is not None:
-        vf_parameters = "fps=fps={}:round=up,zscale=t=linear:npl=100,format=gbrpf32le,zscale=p=bt709,tonemap=tonemap=hable:desat=0,zscale=t=bt709:m=bt709:r=tv,format=yuv420p,scale=w=320:h=240:force_original_aspect_ratio=decrease".format(round(1 / PLEX_BIF_FRAME_INTERVAL, 6))
+        vf_parameters = "fps=fps={}:round=up,zscale=t=linear:npl=100,format=gbrpf32le,zscale=p=bt709,tonemap=tonemap=hable:desat=0,zscale=t=bt709:m=bt709:r=tv,format=yuv420p,scale=w=320:h=240:force_original_aspect_ratio=decrease".format(round(1 / config.PLEX_BIF_FRAME_INTERVAL, 6))
 
     args = [
         FFMPEG_PATH, "-loglevel", "info", "-skip_frame:v", "nokey", "-threads:0", "1", "-i",
-        video_file, "-an", "-sn", "-dn", "-q:v", str(THUMBNAIL_QUALITY),
+        video_file, "-an", "-sn", "-dn", "-q:v", str(config.THUMBNAIL_QUALITY),
         "-vf",
         vf_parameters, '{}/img-%06d.jpg'.format(output_folder)
     ]
@@ -99,7 +84,7 @@ def generate_images(video_file_param, output_folder, lock):
     with lock:
         gpu = gpustat.core.new_query()[0]
         gpu_ffmpeg = [c for c in gpu.processes if c["command"].lower() == 'ffmpeg']
-        if len(gpu_ffmpeg) < GPU_THREADS or CPU_THREADS == 0:
+        if len(gpu_ffmpeg) < config.GPU_THREADS or config.CPU_THREADS == 0:
             hw = True
             args.insert(5, "-hwaccel")
             args.insert(6, "cuda")
@@ -125,7 +110,7 @@ def generate_images(video_file_param, output_folder, lock):
     # Optimize and Rename Images
     for image in glob.glob('{}/img*.jpg'.format(output_folder)):
         frame_no = int(os.path.basename(image).strip('-img').strip('.jpg')) - 1
-        frame_second = frame_no * PLEX_BIF_FRAME_INTERVAL
+        frame_second = frame_no * config.PLEX_BIF_FRAME_INTERVAL
         os.rename(image, os.path.join(output_folder, '{:010d}.jpg'.format(frame_second)))
 
 
@@ -145,7 +130,7 @@ def generate_bif(bif_filename, images_path):
     array.array('B', magic).tofile(f)
     f.write(struct.pack("<I", version))
     f.write(struct.pack("<I", len(images)))
-    f.write(struct.pack("<I", 1000 * PLEX_BIF_FRAME_INTERVAL))
+    f.write(struct.pack("<I", 1000 * config.PLEX_BIF_FRAME_INTERVAL))
     array.array('B', [0x00 for x in range(20, 64)]).tofile(f)
 
     bif_table_size = 8 + (8 * len(images))
@@ -174,7 +159,7 @@ def generate_bif(bif_filename, images_path):
 def process_item(item_key, lock):
     sess = requests.Session()
     sess.verify = False
-    plex = PlexServer(PLEX_URL, PLEX_TOKEN, session=sess)
+    plex = PlexServer(config.PLEX_URL, config.PLEX_TOKEN, session=sess)
 
     data = plex.query('{}/tree'.format(item_key))
 
@@ -186,10 +171,10 @@ def process_item(item_key, lock):
                     return
             bundle_hash = media_part.attrib['hash']
             bundle_file = '{}/{}{}'.format(bundle_hash[0], bundle_hash[1::1], '.bundle')
-            bundle_path = os.path.join(PLEX_LOCAL_MEDIA_PATH, bundle_file)
+            bundle_path = os.path.join(config.PLEX_LOCAL_MEDIA_PATH, bundle_file)
             indexes_path = os.path.join(bundle_path, 'Contents', 'Indexes')
             index_bif = os.path.join(indexes_path, 'index-sd.bif')
-            tmp_path = os.path.join(TMP_FOLDER, bundle_hash)            
+            tmp_path = os.path.join(config.TMP_FOLDER, bundle_hash)
             if (not os.path.isfile(index_bif)) and (not os.path.isdir(tmp_path)):
                 if not os.path.isdir(indexes_path):
                     os.mkdir(indexes_path)
@@ -208,18 +193,19 @@ def process_item(item_key, lock):
 
 
 def run():
-    process_pool = ProcessPoolExecutor(max_workers=CPU_THREADS + GPU_THREADS)
+    process_pool = ProcessPoolExecutor(max_workers=config.CPU_THREADS + config.GPU_THREADS)
 
     # Ignore SSL Errors
     sess = requests.Session()
     sess.verify = False
 
-    plex = PlexServer(PLEX_URL, PLEX_TOKEN, session=sess)
+    plex = PlexServer(config.PLEX_URL, config.PLEX_TOKEN, session=sess)
 
     # Get all Movies
     logger.info('Getting Movies from Plex')
     movies = [m.key for m in plex.library.search(libtype='movie')]
     logger.info('Got {} Movies from Plex', len(movies))
+    sys.exit()
 
     m = multiprocessing.Manager()
     lock = m.Lock()
@@ -248,24 +234,24 @@ if __name__ == '__main__':
     # Also forcing 'colorize=True' otherwise Loguru won't recognize that the sink support colors
     logger.add(lambda m: console.print('\n%s' % m, end=""), colorize=True)
 
-    if not os.path.exists(PLEX_LOCAL_MEDIA_PATH):
-        logger.error('%s does not exist, please edit PLEX_LOCAL_MEDIA_PATH variable' % PLEX_LOCAL_MEDIA_PATH)
+    if not os.path.exists(config.PLEX_LOCAL_MEDIA_PATH):
+        logger.error('%s does not exist, please edit PLEX_LOCAL_MEDIA_PATH variable' % config.PLEX_LOCAL_MEDIA_PATH)
         exit(1)
 
-    if 'xxxxxx' in PLEX_URL:
-        logger.error('Please update the PLEX_URL variable within this script')
+    if 'xxxxxx' in config.PLEX_URL:
+        logger.error('Please update the PLEX_URL variable inside config.py')
         exit(1)
 
-    if 'xxxxxx' in PLEX_TOKEN:
-        logger.error('Please update the PLEX_TOKEN variable within this script')
+    if 'xxxxxx' in config.PLEX_TOKEN:
+        logger.error('Please update the PLEX_TOKEN variable inside config.py')
         exit(1)
 
     try:
         # Clean TMP Folder
-        if os.path.isdir(TMP_FOLDER):
-            shutil.rmtree(TMP_FOLDER)   
-        os.mkdir(TMP_FOLDER)
+        if os.path.isdir(config.TMP_FOLDER):
+            shutil.rmtree(config.TMP_FOLDER)
+        os.mkdir(config.TMP_FOLDER)
         run()
     finally:
-        if os.path.isdir(TMP_FOLDER):
-            shutil.rmtree(TMP_FOLDER)
+        if os.path.isdir(config.TMP_FOLDER):
+            shutil.rmtree(config.TMP_FOLDER)
